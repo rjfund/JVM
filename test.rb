7eded5870516835e32657426dd0168ebfe2b2878
Cooper Mayne <coopermayne@gmail.com>
2017-10-03 16:42:14 -0700

    try to post to other app

--- test.rb
+++ test.rb
@@ -9,14 +9,13 @@ get '/hello-monkey' do
     r.pause(length: 2)
     r.play(digits: "5")
     r.say("start your message")
-    r.record(action: '/record-it', method: 'get', finish_on_key: '*')
+    r.record(action: 'http://court-tracker.herokuapp.com/voice_messages', method: 'post', finish_on_key: '*')
   end.to_s
 end
 
 get '/record-it' do
   Twilio::TwiML::VoiceResponse.new do |r|
     r.say('Listen to your message.')
-    puts params
     r.say('Goodbye.')
   end.to_s
 end
