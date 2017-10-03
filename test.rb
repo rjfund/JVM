04d9e9b85bdeaab0940fa609f2093e95d46e6fe9
Cooper Mayne <coopermayne@gmail.com>
2017-10-03 15:10:37 -0700

    add recording functionality

--- test.rb
+++ test.rb
@@ -7,8 +7,9 @@ get '/hello-monkey' do
   Twilio::TwiML::VoiceResponse.new do |r|
     r.say("Hello Cooper")
     r.pause(length: 5)
-    r.play(digits: "5555")
-    r.say("I played five five times")
+    r.play(digits: "5")
+    r.say("start your message")
+    r.record(timeout: 15, transcribe: true, action: 'http://foo.edu/handleRecording.php', finish_on_key: '*')
   end.to_s
 
 end
