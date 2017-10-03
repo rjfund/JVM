a085fa44e85d733d651dd39e74c655c2bec70aa7
Cooper Mayne <coopermayne@gmail.com>
2017-10-03 14:58:11 -0700

    try to dial 5

--- test.rb
+++ test.rb
@@ -3,31 +3,11 @@ require 'twilio-ruby'
 require 'sinatra'
 
 get '/hello-monkey' do
-  people = {
-    '+17078476844' => 'Cooper Mayne',
-    '+14158675310' => 'Boots',
-    '+14158675311' => 'Virgil',
-    '+14158675312' => 'Marcel',
-  }
-
-  name = people[params['From']] || 'Monkey'
 
   Twilio::TwiML::VoiceResponse.new do |r|
-    r.say("Hello #{name}")
-    r.play(url: "http://demo.twilio.com/hellomonkey/monkey.mp3")
-
-    r.gather(:numDigits => '1', :action => '/hello-monkey/handle-gather', :method => 'get') do |g|
-      g.say('To speak to a real monkey, press 1.')
-      g.say('Press any other key to start over.')
-    end
+    r.say("Hello Cooper")
+    r.pause(length: 10)
+    r.play(digits: "5")
   end.to_s
 
-end
-
-get '/hello-monkey/handle-gather' do
-  redirect '/hello-monkey' unless params['Digits'] == '1'
-  Twilio::TwiML::VoiceResponse.new do |r|
-    r.dial(number: '+13103827885') ### Connect the caller to Koko, or your cell
-    r.say('The call failed or the remote party hung up. Goodbye.')
-  end.to_s
 end
